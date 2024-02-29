5d7545183c349616ac6ff99b3b065747e50557aa
MoussaDIALLO <mousse.md10@gmail.com>
2024-02-29 12:55:39 +0000

    first commit

---
+++ src/main/java/com/groupeisi/graphqletudiant/GraphQlEtudiantApplication.java
@@ -0,0 +1,13 @@
+package com.groupeisi.graphqletudiant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlEtudiantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlEtudiantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/groupeisi/graphqletudiant/GraphQlEtudiantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupeisi.graphqletudiant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlEtudiantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
